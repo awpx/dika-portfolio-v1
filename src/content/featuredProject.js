82cca967c5eed23cee5b4b950b2814cc3bfe4641
awp <awpxwork@gmail.com>
2020-10-09 21:24:00 +0700

    edit some

--- src/content/featuredProject.js
+++ src/content/featuredProject.js
@@ -6,7 +6,7 @@ module.exports = {
       cover: 'images/shop.png',
       github: 'https://github.com/awpx/shop-dika-dev-v1',
       external: 'https://shop.dika.dev',
-      details: 'https://blog.dika.dev',
+      details: 'https://blog.dika.dev/post/5f8e80c4f5bb9349bbfab80b',
       tech: ['ReactJS', 'Redux', 'React-Bootstrap', 'ExpressJS', 'MongoDB(Atlas)', 'AWS', 'JWT', 'Paypal(Sandbox)'],
       description: 'A Shop is a responsive eCommerce website built with React, Redux, React Bootstrap, backend using express, mongoDB Atlas for data storage, deployed in AWS EC2 and other technologies to fulfill the need of customers. A Shop comes with some features, include review system, authentication token (JWT) for security, admin menu, carousel of best product, etc.'
     },
@@ -16,7 +16,7 @@ module.exports = {
       cover: 'images/blog.png',
       github: 'https://github.com/awpx/blog-dika-dev-v1',
       external: 'https://blog.dika.dev',
-      details: 'https://blog.dika.dev',
+      details: 'https://blog.dika.dev/post/5f8e8ea1f5bb9349bbfab80d',
       tech: ['ReactJS', 'Redux', 'React-Bootstrap', 'React-Quill', 'ExpressJS', 'MongoDB(Atlas)', 'AWS', 'JWT'],
       description: 'My personal blog website built with React, Redux, React Bootstrap, React-Quill (text editor), backend using express, mongoDB Atlas for data storage, deployed in AWS EC2, and react quill for text editor.'
     },
@@ -26,7 +26,7 @@ module.exports = {
       cover: 'images/link.png',
       github: 'https://github.com/awpx/link-dika-dev-v1',
       external: 'https://link.dika.dev',
-      details: 'https://blog.dika.dev',
+      details: 'https://blog.dika.dev/post/5f8e8ca4f5bb9349bbfab80c',
       tech: ['ReactJS', 'ExpressJS', 'MongoDB(Atlas)', 'AWS'],
       description: 'Some simple Link/URL Shortener website built with React, backend using express for API, mongoDB Atlas for data storage, deployed in AWS EC2.'
     },
